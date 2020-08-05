2baad571f27fb601ffcb92fce29d1167f112193d
choktn@gmail.com <choktn@gmail.com>
2020-08-05 13:17:41 -0700

    Implemented item id retrieval of the highest pick rate items

---
+++ Generator.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace League_Itemset_Generator
+{
+    class Generator
+    {
+        static void Main(string[] args)
+        {
+            var html = @"https://na.op.gg/champion/aatrox/statistics/top";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var nodes = htmlDoc.DocumentNode.SelectNodes("//tr[@class='champion-overview__row champion-overview__row--first']//img");
+
+            foreach (HtmlNode node in nodes)
+            {
+                string value = node.GetAttributeValue("src", "nothin");
+                MatchCollection mc = Regex.Matches(value, @"\b(\d{4})\b");
+
+                Console.WriteLine(value);
+
+                foreach (Match m in mc)
+                {
+                    Console.WriteLine(m);
+                }
+                
+            }
+            Console.WriteLine(nodes.Count());
+        }
+    }
+}
