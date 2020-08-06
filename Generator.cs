273e556abe70de1b50e30c5bc57e5ba210fa9da6
choktn@gmail.com <choktn@gmail.com>
2020-08-05 19:48:19 -0700

    Added retrieval of non highest pick rate items

--- Generator.cs
+++ Generator.cs
@@ -1,37 +1,82 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace League_Itemset_Generator
 {
+    
+
     class Generator
     {
         static void Main(string[] args)
         {
-            var html = @"https://na.op.gg/champion/aatrox/statistics/top";
+            var html = @"https://na.op.gg/champion/aatrox/statistics/mid";
 
             HtmlWeb web = new HtmlWeb();
 
             var htmlDoc = web.Load(html);
 
-            var nodes = htmlDoc.DocumentNode.SelectNodes("//tr[@class='champion-overview__row champion-overview__row--first']//img");
 
+            List<Category> categories = new List<Category>()
+            {
+                new Category(htmlDoc: htmlDoc, title: "Most Frequent Starters", xpath: "//text()[contains(., 'Starter Items')]/ancestor::tr[1]//img"),
+                new Category(htmlDoc: htmlDoc, title: "Most Frequent Core Build", xpath: "//text()[contains(., 'Recommended Builds')]/ancestor::tr[1]//img"),
+                new Category(htmlDoc: htmlDoc, title: "Most Frequent Boots", xpath: "//text()[contains(., 'Boots')]/ancestor::tr[1]//img"),
+                new Category(htmlDoc: htmlDoc, title: "Other Items", xpath: "//text()[contains(., 'Starter Items')]/ancestor::tbody[1]//img")
+            };
+                      
+            foreach (Category category in categories)
+            {
+                Console.WriteLine(category.type);
+
+                List<string> ids = category.ids;
+                foreach (string id in ids)
+                {
+                    Console.WriteLine(id);
+                }
+            }
+                        
+        }
+    }
+
+    static class Ids
+    {
+        public static List<string> idList = new List<string>();
+    }
+
+    class Category
+    {
+        public string type;
+
+        public List<string> ids;
+
+        public Category(HtmlDocument htmlDoc, string title, string xpath)
+        {
+            type = title;
+
+            ids = new List<string>();
+
+            var nodes = htmlDoc.DocumentNode.SelectNodes(xpath);
             foreach (HtmlNode node in nodes)
             {
-                string value = node.GetAttributeValue("src", "nothin");
+                string value = node.GetAttributeValue("src", "nothing");
                 MatchCollection mc = Regex.Matches(value, @"\b(\d{4})\b");
-
-                Console.WriteLine(value);
 
                 foreach (Match m in mc)
                 {
-                    Console.WriteLine(m);
+                    string id = m.ToString();
+
+                    if (!Ids.idList.Contains(id))
+                    {
+                        ids.Add(id);
+                        Ids.idList.Add(id);
+                    }
+                    
                 }
-                
             }
-            Console.WriteLine(nodes.Count());
         }
     }
 }
+
+
