80f07274094db7527d49b0f462074d3bea0f3d6c
choktn@gmail.com <choktn@gmail.com>
2020-08-07 12:55:36 -0700

    Added Skills upgrade order

--- Generator.cs
+++ Generator.cs
@@ -2,33 +2,34 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace League_Itemset_Generator
 {
-    
+
     class Generator
     {
         static void Main(string[] args)
         {
-           /* WebClient client = new WebClient();
-            Stream stream = client.OpenRead("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/Aatrox.json");
-            StreamReader reader = new StreamReader(stream);
+            /* WebClient client = new WebClient();
+             Stream stream = client.OpenRead("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/Aatrox.json");
+             StreamReader reader = new StreamReader(stream);
 
-            dynamic DynamicData = JsonConvert.DeserializeObject(reader.ReadLine());
+             dynamic DynamicData = JsonConvert.DeserializeObject(reader.ReadLine());
 
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items.Count);
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[1].id);
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[2].id);
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[0].id);
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[1].id);
-            Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[2].id);*/
-
-            Console.WriteLine(JsonConvert.SerializeObject(new ItemSet("Thresh", "support")));
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items.Count);
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[1].id);
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[2].id);
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[0].id);
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[1].id);
+             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[2].id);*/
+          
+            Console.WriteLine(JsonConvert.SerializeObject(new ItemSet("Anivia", "Mid")));
         }
     }
 
-    class ItemSet
+        class ItemSet
     {
         public string map = "any";
 
@@ -52,10 +53,10 @@ namespace League_Itemset_Generator
             title = name + " " + lane;
             champion = name.ToLower();
 
-            foreach (var category in XPath.paths)
-            {
-                blocks.Add(new Category(name: category.Key, champion: champion, lane: lane));
-            }
+            blocks.Add(new Category(name: "Starters", champion: champion, lane: lane));
+            blocks.Add(new Category(name: "Core Build", champion: champion, lane: lane));
+            blocks.Add(new Category(name: "Other Items", champion: champion, lane: lane));
+
         }
     }
 
@@ -71,6 +72,44 @@ namespace League_Itemset_Generator
 
             HtmlDocument htmlDoc = new HtmlWeb().Load("https://na.op.gg/champion/" + champion + "/statistics/" + lane);
 
+            if (type == "Starters")
+            {
+                HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Skill Order"]);
+
+                type += " | Skills: ";
+                for (int i = 0; i < 4; i++)
+                {
+                    type += nodes[i].InnerText.Trim();
+                    if (i < 3)
+                    {
+                        type += ".";
+                    }
+                }
+                
+                nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Upgrade Order"]);
+
+                type += " - ";
+                for (int i = 0; i < 3; i++)
+                {
+                    type += nodes[i].InnerText;
+                    if (i < 2)
+                    {
+                        type += ">";
+                    }
+                }
+            }
+            else if (type == "Core Build")
+            {
+                HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Most Common Boot"]);
+                MatchCollection regex = Regex.Matches(nodes[0].GetAttributeValue("src", "nothing"), @"\b(\d{4})\b");
+                Item newitem = new Item();
+
+                newitem.id = regex[0].ToString();
+                newitem.count = 1;
+                items.Add(newitem);
+            }
+
+
             foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes(XPath.paths[name]))
             {
                 MatchCollection regex = Regex.Matches(node.GetAttributeValue("src", "nothing"), @"\b(\d{4})\b");
@@ -78,9 +117,12 @@ namespace League_Itemset_Generator
 
                 foreach (Match id in regex)
                 {
-                    newitem.id = id.ToString();
-                    newitem.count = 1;
-                    items.Add(newitem);
+                    if (!items.Any(n => n.id == id.ToString()))
+                    {
+                        newitem.id = id.ToString();
+                        newitem.count = 1;
+                        items.Add(newitem);
+                    }
                 }
             }
         }
@@ -96,9 +138,11 @@ namespace League_Itemset_Generator
     {
         public static Dictionary<string, string> paths = new Dictionary<string, string>()
             {
-                {"Most Frequent Starters", "//text()[contains(., 'Starter Items')]/ancestor::tr[1]//img"},
-                {"Most Frequent Core Build","//text()[contains(., 'Recommended Builds')]/ancestor::tr[1]//img"},
-                {"Most Frequent Boots", "//text()[contains(., 'Boots')]/ancestor::tr[1]//img"},
+                {"Starters", "//text()[contains(., 'Starter Items')]/ancestor::tr[1]//img"},
+                {"Skill Order", "//table[@class='champion-skill-build__table']//td"},
+                {"Upgrade Order",  "//table[@class='champion-skill-build__table']//td/ancestor::td[1]//ul//span"},
+                {"Core Build","//text()[contains(., 'Recommended Builds')]/ancestor::tr[1]//img"},
+                {"Most Common Boot", "//text()[contains(., 'Boots')]/ancestor::tr[1]//img"},
                 {"Other Items", "//text()[contains(., 'Starter Items')]/ancestor::tbody[1]//img"}/*,
                 {"Recommended Skill Builds", "//li[@class='champion-stats__list__item tip tpd-delegation-uid-1']//img"}*/
             };
