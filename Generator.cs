29d6801208907ccfb1eb370fa8572a18e2dfd81f
choktn@gmail.com <choktn@gmail.com>
2020-08-07 19:22:58 -0700

    Added outputting of json text to file format/item organization

--- Generator.cs
+++ Generator.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -12,20 +13,14 @@ namespace League_Itemset_Generator
     {
         static void Main(string[] args)
         {
-            /* WebClient client = new WebClient();
-             Stream stream = client.OpenRead("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/Aatrox.json");
-             StreamReader reader = new StreamReader(stream);
+            string json = JsonConvert.SerializeObject(new ItemSet("Anivia", "Mid"));
 
-             dynamic DynamicData = JsonConvert.DeserializeObject(reader.ReadLine());
-
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items.Count);
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[1].id);
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[2].id);
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[0].id);
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[1].id);
-             Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[2].id);*/
-          
-            Console.WriteLine(JsonConvert.SerializeObject(new ItemSet("Anivia", "Mid")));
+            using (var tw = new StreamWriter(@"C:\Users\tavinc\Documents\test.json", false))
+            {
+                tw.WriteLine(json);
+                tw.Close();
+            }
+            Console.WriteLine(json);
         }
     }
 
@@ -47,84 +42,143 @@ namespace League_Itemset_Generator
 
         public string champion;
 
-        public ItemSet(string name, string lane)
+        public ItemSet(string name, string pos)
         {
             blocks = new List<Category>();
-            title = name + " " + lane;
+            title = name + " " + pos;
             champion = name.ToLower();
 
-            blocks.Add(new Category(name: "Starters", champion: champion, lane: lane));
-            blocks.Add(new Category(name: "Core Build", champion: champion, lane: lane));
-            blocks.Add(new Category(name: "Other Items", champion: champion, lane: lane));
+            Utility.allItemIds = new List<string>();
 
+            blocks.Add(new Category(name: "Starters", champion: champion, role: pos));
+            blocks.Add(new Category(name: "Core Build", champion: champion, role: pos));
+            blocks.Add(new Category(name: "Other Items", champion: champion, role: pos));
         }
     }
 
     class Category
     {
+        public string type;
+
         public List<Item> items;
 
-        public string type;
-        public Category(string name, string champion, string lane)
+        public Category(string name, string champion, string role)
         {
             items = new List<Item>();
             type = name;
 
-            HtmlDocument htmlDoc = new HtmlWeb().Load("https://na.op.gg/champion/" + champion + "/statistics/" + lane);
+            HtmlDocument htmlDoc = new HtmlWeb().Load($"https://na.op.gg/champion/{champion}/statistics/{role}");
 
-            if (type == "Starters")
+            switch (type)
             {
-                HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Skill Order"]);
+                case "Starters":
+                    HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(Utility.paths["Skill Order"]);
 
-                type += " | Skills: ";
-                for (int i = 0; i < 4; i++)
-                {
-                    type += nodes[i].InnerText.Trim();
-                    if (i < 3)
+                    type += " | Skills: ";
+                    for (int i = 0; i < 4; i++)
                     {
-                        type += ".";
+                        type += nodes[i].InnerText.Trim();
+                        if (i < 3)
+                        {
+                            type += ".";
+                        }
                     }
-                }
-                
-                nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Upgrade Order"]);
 
-                type += " - ";
-                for (int i = 0; i < 3; i++)
-                {
-                    type += nodes[i].InnerText;
-                    if (i < 2)
+                    nodes = htmlDoc.DocumentNode.SelectNodes(Utility.paths["Upgrade Order"]);
+
+                    type += " - ";
+                    for (int i = 0; i < 3; i++)
                     {
-                        type += ">";
+                        type += nodes[i].InnerText;
+                        if (i < 2)
+                        {
+                            type += ">";
+                        }
                     }
-                }
+
+                    foreach (string id in getItemIds(htmlDoc, name).Reverse<string>())
+                    {
+                        Item newItem = new Item();
+
+                        newItem.id = id;
+                        newItem.count = (id == "2003") ? 2 : 1;
+
+                        items.Add(newItem);
+                    }
+
+                    break;
+
+                case "Core Build":
+                    foreach (string id in getItemIds(htmlDoc, "Most Common Boot").Concat(getItemIds(htmlDoc, name)))
+                    {
+                        Item newItem = new Item();
+
+                        newItem.id = id;
+                        newItem.count = 1;
+
+                        items.Add(newItem);
+                    }
+
+                    break;
+
+                case "Other Items":
+                    foreach (string id in getItemIds(htmlDoc, name))
+                    {
+                        Item newItem = new Item();
+
+                        newItem.id = id;
+                        newItem.count = 1;
+
+                        items.Add(newItem);
+                    }
+
+                    Item temp = items[0];
+                    items[0] = items[items.Count-2];
+                    items[items.Count - 2] = temp;
+
+                    temp = items[1];
+                    items[1] = items[items.Count - 1];
+                    items[items.Count - 1] = temp;
+
+                    /* WebClient client = new WebClient();
+                    Stream stream = client.OpenRead("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/Aatrox.json");
+                    StreamReader reader = new StreamReader(stream);
+
+                    dynamic DynamicData = JsonConvert.DeserializeObject(reader.ReadLine());
+
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items.Count);
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[1].id);
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[6].items[2].id);
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[0].id);
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[1].id);
+                    Console.WriteLine(DynamicData.data.Aatrox.recommended[4].blocks[7].items[2].id);*/
+
+                    break;
             }
-            else if (type == "Core Build")
-            {
-                HtmlNodeCollection nodes = htmlDoc.DocumentNode.SelectNodes(XPath.paths["Most Common Boot"]);
-                MatchCollection regex = Regex.Matches(nodes[0].GetAttributeValue("src", "nothing"), @"\b(\d{4})\b");
-                Item newitem = new Item();
+        }
 
-                newitem.id = regex[0].ToString();
-                newitem.count = 1;
-                items.Add(newitem);
-            }
+        List<string> getItemIds(HtmlDocument htmlDoc, string itemtype)
+        {
+            List<String> itemIds = new List<String>();
 
-
-            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes(XPath.paths[name]))
+            foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes(Utility.paths[itemtype]))
             {
                 MatchCollection regex = Regex.Matches(node.GetAttributeValue("src", "nothing"), @"\b(\d{4})\b");
-                Item newitem = new Item();
 
-                foreach (Match id in regex)
+                if (regex.Count > 0)
                 {
-                    if (!items.Any(n => n.id == id.ToString()))
+                    string id = regex[0].ToString();
+
+                    if (!Utility.allItemIds.Contains(id))
                     {
-                        newitem.id = id.ToString();
-                        newitem.count = 1;
-                        items.Add(newitem);
+                        itemIds.Add(id);
+
+                        Utility.allItemIds.Add(id);
                     }
                 }
             }
+
+            return itemIds;
         }
     }
 
@@ -134,7 +188,7 @@ namespace League_Itemset_Generator
         public int count { get; set; }
     }
 
-    static class XPath
+    static class Utility
     {
         public static Dictionary<string, string> paths = new Dictionary<string, string>()
             {
@@ -143,10 +197,13 @@ namespace League_Itemset_Generator
                 {"Upgrade Order",  "//table[@class='champion-skill-build__table']//td/ancestor::td[1]//ul//span"},
                 {"Core Build","//text()[contains(., 'Recommended Builds')]/ancestor::tr[1]//img"},
                 {"Most Common Boot", "//text()[contains(., 'Boots')]/ancestor::tr[1]//img"},
-                {"Other Items", "//text()[contains(., 'Starter Items')]/ancestor::tbody[1]//img"}/*,
-                {"Recommended Skill Builds", "//li[@class='champion-stats__list__item tip tpd-delegation-uid-1']//img"}*/
+                {"Other Items", "//text()[contains(., 'Recommended Builds')]/ancestor::tbody[1]//img"}
             };
+
+        public static List<string> allItemIds;
     }
+
+
 }
 
 
